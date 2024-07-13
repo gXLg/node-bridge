ae865306375a34d1842a549c18270bd660cb6d42
gXLg <natgaev@gmail.com>
2024-07-13 02:00:25 +0200

    Verbosity

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 (async () => {
 
+  // imports
   const tunnel = require("./lib/tunnel.js");
-  tunnel.test();
-  const uuid = tunnel.init();
-
   const dns = require("./lib/dns.js");
-  const old_records = dns.init();
-
   const servers = require("./lib/servers.js");
-  const { new_records, runlist } = servers.init();
-
-  const services = await dns.update(old_records, new_records, uuid);
-
   const runner = require("./lib/runner.js");
 
+
+  // test for the binary
+  tunnel.test();
+
+  // init tunnel config
+  const uuid = tunnel.init();
+
+  // read old hostname settings
+  const old_records = dns.init();
+
+  // parse the servers in the servers/ folder
+  const { new_records, runlist } = servers.init();
+
+  // update hostname records
+  const services = await dns.update(old_records, new_records, uuid);
+
+  // start the services one after eachother
   for (const record in runlist) {
     const config = runlist[record];
-
     const start = config.run;
     const stop = config.stop;
     const cwd = config.dir;
@@ -25,6 +33,7 @@
     runner.start(start, stop, cwd, port);
   }
 
+  // start the tunnel
   tunnel.run();
 
 })();
